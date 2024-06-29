f159a9e3c64bb1ea35c14f0fc32c40cbd2e0fb05
Lý Anh Hào <haoly01265820192@gmail.com>
2024-06-30 01:16:50 +0700

    Update
    Add setting sound UI

--- Assets/AudioManager.cs
+++ Assets/AudioManager.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
     [Header("-----AudioGameObject-----")]
-
+    [SerializeField] AudioMixer audioMixer;
     [SerializeField] AudioSource backGroundMusicSource;
     [SerializeField] AudioSource SFX;
 
@@ -38,5 +39,25 @@ public class AudioManager : MonoBehaviour
     public void PlaySFX(AudioClip audioClip)
     {
         SFX.PlayOneShot(audioClip);
+    }
+
+    public void SetMasterVolume(float level)
+    {
+        audioMixer.SetFloat("MasterVolume", Mathf.Log10(level)*20);
+    }
+
+    public void SetMusicVolume(float level)
+    {
+        audioMixer.SetFloat("MusicVolume", Mathf.Log10(level) * 20);
+    }
+
+    public void SetSoundFXVolume(float level)
+    {
+        audioMixer.SetFloat("SoundFXVolume", Mathf.Log10(level) * 20);
+    }
+
+    public void SetSpecVolume(float level)
+    {
+        audioMixer.SetFloat("SpecVolume", Mathf.Log10(level) * 20);
     }
 }

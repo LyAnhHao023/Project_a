4a463bafa19b98e81191e8ce80f5221eb83dcae3
Đặng Hoàn Phương <danghoanphuong11052003@gmail.com>
2024-07-17 11:14:37 +0700

    fix

--- Assets/Script/ShopManager/GoodsManager.cs
+++ Assets/Script/ShopManager/GoodsManager.cs
@@ -18,14 +18,16 @@ public class GoodsManager : MonoBehaviour
             goodsHolder = Instantiate(goodsPerfab, goodsTranform.transform);
 
             //if ((int)goodsDatas[i].type == 0)
-                goodsDatas[i].level = PlayerPrefs.GetInt(goodsDatas[i].goodsName + "lv", 0);
 
-            /*if ((int)goodsDatas[i].type == 0)
-            {
-                PlayerPrefs.SetInt(goodsDatas[i].goodsName + "lv", 0);
-                PlayerPrefs.Save();
-            }*/
 
+            //if ((int)goodsDatas[i].type == 0)
+            //{
+                
+            //}
+
+            PlayerPrefs.SetInt(goodsDatas[i].goodsName + "lv", 0);
+            PlayerPrefs.Save();
+            goodsDatas[i].level = PlayerPrefs.GetInt(goodsDatas[i].goodsName + "lv", 0);
             goodsHolder.GetComponent<SetItemShop>().Set(goodsDatas[i]);
         }
     }
